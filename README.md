a9db8cb718d1c3381f61cf2e081b308c98c3da20
erik-tastepe <erik.tastepe@gmail.com>
2021-01-23 16:35:06 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# anarcholib cli
+
+## What is it?
+A command-line application for querying and downloading from the Anarchism Library
---
+++ setup.py
@@ -0,0 +1,76 @@
+from setuptools import setup, find_packages
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+with open(path.join(here, "README.md"), encoding="utf-8") as f:
+    long_description = f.read()
+
+setup(
+    name="anarcholib_cli",  
+    version="1.0.0",
+    description="A command line application to query and download from the Anarchist Library.",
+    long_description=long_description, 
+    long_description_content_type="text/markdown", 
+    url="https://github.com/erik-tastepe/anarcholib_cli",
+    author="Erik Tastepe", 
+    author_email="erik.tastepe@gmail.com", 
+    classifiers=[  
+        "Development Status :: 2 - Pre-Alpha",
+        "Intended Audience :: Science/Research",
+        "Topic :: Education",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9"
+    ],
+    keywords="anarchism cli",  
+    packages=find_packages(exclude=["contrib", "docs", "tests"]), 
+    python_requires=">=3.5",
+    install_requires=[],  
+    extras_require={
+        "dev": [
+            "appdirs==1.4.4",
+            "attrs==20.3.0; python_version >= '2.7' and python_version not in '3.0, 3.1, 3.2, 3.3'",
+            "black==19.10b0; python_version >= '3.6'",
+            "cached-property==1.5.2",
+            "cerberus==1.3.2",
+            "certifi==2020.12.5",
+            "chardet==4.0.0; python_version >= '2.7' and python_version not in '3.0, 3.1, 3.2, 3.3, 3.4'",
+            "click==7.1.2; python_version >= '2.7' and python_version not in '3.0, 3.1, 3.2, 3.3, 3.4'",
+            "colorama==0.4.4; python_version >= '2.7' and python_version not in '3.0, 3.1, 3.2, 3.3, 3.4'",
+            "distlib==0.3.1",
+            "idna==2.10; python_version >= '2.7' and python_version not in '3.0, 3.1, 3.2, 3.3'",
+            "iniconfig==1.1.1",
+            "orderedmultidict==1.0.1",
+            "packaging==20.8; python_version >= '2.7' and python_version not in '3.0, 3.1, 3.2, 3.3'",
+            "pathspec==0.8.1",
+            "pep517==0.9.1",
+            "pip-shims==0.5.3; python_version >= '2.7' and python_version not in '3.0, 3.1, 3.2, 3.3, 3.4'",
+            "pipenv-setup==3.1.1",
+            "pipfile==0.0.2",
+            "plette[validation]==0.2.3; python_version >= '2.6' and python_version not in '3.0, 3.1, 3.2, 3.3'",
+            "pluggy==0.13.1; python_version >= '2.7' and python_version not in '3.0, 3.1, 3.2, 3.3'",
+            "py==1.10.0; python_version >= '2.7' and python_version not in '3.0, 3.1, 3.2, 3.3'",
+            "pyparsing==2.4.7; python_version >= '2.6' and python_version not in '3.0, 3.1, 3.2, 3.3'",
+            "pytest==6.2.1",
+            "python-dateutil==2.8.1; python_version >= '2.7' and python_version not in '3.0, 3.1, 3.2, 3.3'",
+            "regex==2020.11.13",
+            "requests==2.25.1; python_version >= '2.7' and python_version not in '3.0, 3.1, 3.2, 3.3, 3.4'",
+            "requirementslib==1.5.16; python_version >= '2.7' and python_version not in '3.0, 3.1, 3.2, 3.3, 3.4'",
+            "six==1.15.0; python_version >= '2.7' and python_version not in '3.0, 3.1, 3.2, 3.3'",
+            "toml==0.10.2; python_version >= '2.6' and python_version not in '3.0, 3.1, 3.2, 3.3'",
+            "tomlkit==0.7.0; python_version >= '2.7' and python_version not in '3.0, 3.1, 3.2, 3.3, 3.4'",
+            "typed-ast==1.4.2",
+            "urllib3==1.26.2; python_version >= '2.7' and python_version not in '3.0, 3.1, 3.2, 3.3, 3.4' and python_version < '4'",
+            "vistir==0.5.2; python_version >= '2.7' and python_version not in '3.0, 3.1, 3.2, 3.3'",
+            "wheel==0.36.2; python_version >= '2.7' and python_version not in '3.0, 3.1, 3.2, 3.3, 3.4'",
+        ]
+    },  
+    dependency_links=[],
+    project_urls={ 
+        "Source": "https://github.com/erik-tastepe/anarcholib_cli",
+    },
+)
